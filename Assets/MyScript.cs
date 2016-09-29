8e0224ff087f0bce6b959ce3b1217ae0ff491d34
playablegraphs <playablegraphs@gmail.com>
2016-09-29 17:25:42 +0300

    Added MyScript.cs file

---
+++ Assets/MyScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("The First Print");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
