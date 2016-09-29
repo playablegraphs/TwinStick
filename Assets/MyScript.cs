c5fbcd444957d1c0c864cd1e4023b12fe70fb2dd
playablegraphs <playablegraphs@gmail.com>
2016-09-29 17:27:17 +0300

    Added Something Else

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -6,6 +6,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("The First Print");
+		print ("The Second Print");
 	}
 	
 	// Update is called once per frame
