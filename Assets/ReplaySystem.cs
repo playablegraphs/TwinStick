b9f9f7ee4804a5e3f8a974cd1b5343245225aec8
playablegraphs <playablegraphs@gmail.com>
2016-09-29 20:25:22 +0300

    Replay System - Developing Process

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames;
+	private Rigidbody m_Rigidbody;
+	public bool isPlayBack;
+	void Awake (){
+		keyFrames = new MyKeyFrame[bufferFrames];
+	}
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody> ();
+		isPlayBack = false;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!isPlayBack) {
+			Record ();
+		}
+		else if(isPlayBack){
+			PlayBack ();
+		}
+	}
+	void PlayBack(){
+		m_Rigidbody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		this.transform.position = keyFrames [frame].position;
+		this.transform.rotation = keyFrames [frame].rotation;
+		print ("Playing Frame: " + frame);
+	}
+	void Record ()
+	{
+		m_Rigidbody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		keyFrames [frame] = new MyKeyFrame (time, this.transform.position, this.transform.rotation);
+		print ("Recording Frame: " + frame);
+	}
+}
+/// <summary>
+/// A Class for storing time,position, and rotatin.
+/// </summary>
+public class MyKeyFrame{
+	public float time;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame(float time,Vector3 position,Quaternion rotation){
+		this.time = time;
+		this.position = position;
+		this.rotation = rotation;
+	
+	
+	}
+
+	
+}
+
